d8c73de396a64cd8897f4cfca63aaaf372b7d069
camkol <koliyus3@hotmail.com>
2024-08-16 14:42:46 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import React from 'react';
+import Header from "./Header/Header";
+import Todos from "./Todos/Todos";
+
+function App() {
+  return (
+    <main>
+       <Header />
+        <Todos />
+    </main>
+  );
+}
+
+export default App
